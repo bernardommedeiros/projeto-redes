e95b7a56039829f2164b50e16df51cadb8ba265f
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-02-18 23:06:01 -0300

    Update Commands.java

--- src/main/java/org/projetoredes/enums/Commands.java
+++ src/main/java/org/projetoredes/enums/Commands.java
@@ -11,4 +11,17 @@ public enum Commands {
     Commands(String command){
         this.command = command;
     }
+    
+    public String getCommand() {
+        return command;
+    }
+
+    public static Commands fromString(String command) {
+        for (Commands c : Commands.values()) {
+            if (c.getCommand().equalsIgnoreCase(command)) {
+                return c;
+            }
+     }
+     return null;
+    }
 }
